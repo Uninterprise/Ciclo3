ce7e6e2ac02520f33c8c7f9448b74e61fc87ac94
YonnierUni <47702358+YonnierUni@users.noreply.github.com>
2021-10-21 02:28:31 -0500

    cm

--- src/main/java/com/miempresa/aplicacion/AplicacionApplication.java
+++ src/main/java/com/miempresa/aplicacion/AplicacionApplication.java
@@ -2,12 +2,14 @@ package com.miempresa.aplicacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.miempresa.aplicacion.controladores"})
 public class AplicacionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AplicacionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AplicacionApplication.class, args);
+    }
 
 }
